c86969aa976f691624049bfc17619cf5cb703f28
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-11 16:34:50 +0530

    initialize firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const config = {
+    apiKey: "AIzaSyAHum-MwRngFqOy96ja5cpZZrpx3KrD7UM",
+    authDomain: "chat-web-app-4a1c1.firebaseapp.com",
+    projectId: "chat-web-app-4a1c1",
+    storageBucket: "chat-web-app-4a1c1.appspot.com",
+    messagingSenderId: "134685581320",
+    appId: "1:134685581320:web:a1f66dd7459251ec1631bd"
+  };
+
+  const app =firebase.initializeApp(config)
