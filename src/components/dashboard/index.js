de96574f2c09e4faf89f20a65e43ba27fb8e8381
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-14 13:18:37 +0530

    updating mickname

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Drawer,Button } from 'rsuite'
+import { Drawer,Button, Divider } from 'rsuite'
 import { useProfile } from '../../context/profile.context'
+import EditableInput from '../EditableInput'
 
 const Dashboard = ({onSignOut}) =>{
 
     const {profile}=useProfile()
+
+    const onSave = async newData => {
+        console.log(newData)
+    }
 
 return  (
     <>
@@ -17,6 +22,12 @@ return  (
     </Drawer.Header>
     <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
+        <Divider/>
+        <EditableInput
+        name="nickname"
+        initialValue={profile.name}
+        onSave={onSave}
+        label={<h6 className="mb-2">Nickname</h6>}/>
 
     </Drawer.Body>
     <Drawer.Footer>
