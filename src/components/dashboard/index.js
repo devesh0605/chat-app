9c378dd8061fadd08d1f6141937bc36bcdfafbf8
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-14 16:29:55 +0530

    facebook login

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import { Drawer,Button, Divider } from 'rsuite'
+import { Drawer,Button, Divider, Alert } from 'rsuite'
 import { useProfile } from '../../context/profile.context'
+import { database } from '../../misc/firebase'
 import EditableInput from '../EditableInput'
+import ProviderBlock from './ProviderBlock'
 
 const Dashboard = ({onSignOut}) =>{
 
     const {profile}=useProfile()
 
     const onSave = async newData => {
-        console.log(newData)
+        const userNicknameRef = database.ref(`/profiles/${profile.uid}`).child('name')
+        try{
+            await userNicknameRef.set(newData)
+            Alert.success('Nickname has been updated!',4000)
+        }catch (err){
+            Alert.error(err.message,4000)
+        }
+       
     }
 
 return  (
@@ -22,6 +31,7 @@ return  (
     </Drawer.Header>
     <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
+        <ProviderBlock/>
         <Divider/>
         <EditableInput
         name="nickname"
