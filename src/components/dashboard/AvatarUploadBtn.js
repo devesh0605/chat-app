fcfccd1097465af4508a218c436cef25500fe025
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-16 22:02:40 +0530

    updated nickname

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -7,6 +7,7 @@ import { useModalState } from '../../misc/custom-hooks'
 import { useProfile } from '../../context/profile.context'
 import { database, storage } from '../../misc/firebase'
 import ProfileAvatar from '../ProfileAvatar'
+import { getUserUpdates } from '../../misc/helpers'
 
 const fileInputTypes = ".png, .jpeg, .jpg"
 const acceptedFileTypes=['image/png','image/jpeg','image/pjepeg']
@@ -53,9 +54,12 @@ const AvatarUploadBtn = () => {
         const uploadAvatarResult = await avatarFileRef.put(blob,{
             cacheControl:`public,max-age=${3600 * 24 * 3}`
         })
+
         const downloadUrl=await uploadAvatarResult.ref.getDownloadURL()
-        const userAvatarRef=database.ref(`/profiles/${profile.uid}`).child('avatar')
-        userAvatarRef.set(downloadUrl)
+        const updates = await getUserUpdates(profile.uid,'avatar',downloadUrl,database)
+
+        await database.ref().update(updates)
+
         setIsLoading(false)
         Alert.info('Avatar has been uploaded',4000)
         
