4fb7eaa86da69fe1d1bf8d903d3b6c2efdaa92b2
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-17 00:19:50 +0530

    displayed user data

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import TimeAgo from 'timeago-react'
 import ProfileAvatar from '../../ProfileAvatar'
+import ProfileInfoBtnModal from './ProfileInfoBtnModal'
 
 const MessageItem = ({message}) => {
 
@@ -8,14 +9,20 @@ const MessageItem = ({message}) => {
     return (
         <li className="padded mb-1">
             <div className="d-flex align-item-center font-bolder">
-             <ProfileAvatar src={author.avatar} name={author.name} className="ml-1" size="xs"/>
-
-            </div>
-            <div>
-                <span className="word-break-all">{text}</span>
+             <ProfileAvatar 
+             src={author.avatar} 
+             name={author.name} 
+             className="ml-1" 
+             size="xs"/>
+             <ProfileInfoBtnModal profile={author} 
+             appearance="link" className="p-0 ml-1 text-black"/>
                 <TimeAgo
                 datetime={createdAt}
                 className="font-normal text-black-45 ml-2"/>
+            </div>
+            <div>
+            <span className="word-break-all">{text}</span>
+
             </div>
         </li>
         
