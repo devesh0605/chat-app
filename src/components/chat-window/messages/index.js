7ab9e2ebafebc2046d440e40d19733f3b1aacc2a
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-16 13:01:51 +0530

    rooms error occured

---
+++ src/components/chat-window/messages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Messages = () => (
+        <div>
+            Messages
+        </div>
+    )
+
+
+export default Messages
---
+++ src/components/chat-window/top/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Top = () =>  (
+        <div>
+            top
+        </div>
+    )
+
+
+export default Top
